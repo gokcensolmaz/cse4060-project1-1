544ba09289327e0505ed675850bd09462033b477
gokcensolmaz <gokcensolmaz99@gmail.com>
2023-04-15 19:53:49 +0300

    readFile function created. File reading operations move into that function.

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -1,18 +1,29 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class main {
+public class LexicalAnalyzer {
     public static void main(String[] args) {
+        List<String> fileContents = readFile("test.ppll");
+        for (String line : fileContents) {
+            System.out.println(line);
+        }
+    }
+
+    private static List<String> readFile(String fileName) {
+        List<String> fileContents = new ArrayList<>();
+
         try {
             // Step 1: Open the file
-            FileReader fileReader = new FileReader("test.ppll");
+            FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             // Step 2: Read the contents of the file and write to console
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+                fileContents.add(line);
             }
 
             // Step 3: Close the file
@@ -20,5 +31,6 @@ public class main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return fileContents;
     }
 }
